5d0661e3321680a76b191cb0b41c235ce0aa7ad4
martingerardoc <martingerardoc56@gmail.com>
2025-06-28 10:26:09 -0300

    main program modificado

--- tarea1/src/main.jsx
+++ tarea1/src/main.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -8,3 +10,8 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
